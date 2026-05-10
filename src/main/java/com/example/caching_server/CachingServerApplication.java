84a3796de4f74ed9a4601809c5b554a1c5130cf8
Aman Jason <ajt30928@gmail.com>
2026-05-10 20:45:50 +0530

    First commit

---
+++ src/main/java/com/example/caching_server/CachingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.caching_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caching_server/CachingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caching_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
