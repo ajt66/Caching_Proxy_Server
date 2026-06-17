82231434fe5f5af66ca3ae9ad99184d49405e3d4
Aman Jason <ajt30928@gmail.com>
2026-06-17 12:30:07 +0530

    functional changes

--- src/main/java/com/example/caching_server/Controller/ProductsController.java
+++ src/main/java/com/example/caching_server/Controller/ProductsController.java
@@ -3,13 +3,32 @@ package com.example.caching_server.Controller;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import reactor.core.publisher.Mono;
 
-@Controller
+import com.example.caching_server.Service.*;
+
+@RestController
+@RequestMapping("/products")
 public class ProductsController {
+
+    private final ProxyService pservice;
+
+    @Autowired
+    ProductsController(ProxyService pservice) {
+        this.pservice = pservice;
+    }
+
+    @GetMapping("/{id}")
+    public Mono<String> idAPICall(@PathVariable String id) {
+        String baseAPI = "https://dummyjson.com/products";
+        return pservice.defaultAPICall(baseAPI, id);
+    }
 
 }
--- src/main/java/com/example/caching_server/Service/ProxyService.java
+++ src/main/java/com/example/caching_server/Service/ProxyService.java
@@ -2,6 +2,7 @@ package com.example.caching_server.Service;
 
 import com.example.caching_server.Config.*;
 //import com.example.caching_server.model.WeatherResponse;
+import com.example.caching_server.model.products;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,68 +19,30 @@ public class ProxyService {
     // private final UnifiedJedis jedis;
 
     @Autowired
-    public WeatherTrackerService(WebClient.Builder webBuilder, Environment environment) {
+    public ProxyService(WebClient.Builder webBuilder, Environment environment) {
         this.webBuilder = webBuilder;
         this.environment = environment;
     }
 
-    public WeatherResponse dynamicAPICall(String url, String locationParam, String rangeParam,
-            String unitGroupParam) {
-        String endpoint;
-        if (rangeParam.equals("15days")) {
-            endpoint = locationParam;
-        } else {
-            endpoint = locationParam + "/" + rangeParam;
-        }
-
+    public Mono<String> defaultAPICall(String url, String id) {
         try {
             return webBuilder
                     .baseUrl(url)
                     .build()
                     .get()
                     .uri(uriBuilder -> uriBuilder
-                            .path(endpoint)
-                            .queryParam("unitGroup", unitGroupParam)
-                            .queryParam("key", environment.getProperty("env.data.one"))
-                            .queryParam("contentType", "json")
+                            .path(id)
                             .build())
                     .retrieve()
                     .onStatus(status -> status.isError(), clientResponse -> clientResponse.bodyToMono(String.class)
                             .defaultIfEmpty("<no response body>")
                             .flatMap(body -> Mono.error(new RuntimeException(
-                                    "VisualCrossing API failed with status " + clientResponse.statusCode() + ": "
+                                    "Origin Server API failed with status " + clientResponse.statusCode() + ": "
                                             + body))))
-                    .bodyToMono(WeatherResponse.class)
-                    .block();
+                    .bodyToMono(String.class);
         } catch (Exception e) {
             throw new RuntimeException("Weather API call failed: " + e.getMessage(), e);
         }
     }
 
-    public WeatherResponse customAPICall(String url, String locationParam, String date1Param, String date2Param,
-            String unitGroupParam) {
-        String endpoint = locationParam + "/" + date1Param + "/" + date2Param;
-        try {
-            return webBuilder
-                    .baseUrl(url)
-                    .build()
-                    .get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path(endpoint)
-                            .queryParam("unitGroup", unitGroupParam)
-                            .queryParam("key", environment.getProperty("env.data.one"))
-                            .queryParam("contentType", "json")
-                            .build())
-                    .retrieve()
-                    .onStatus(status -> status.isError(), clientResponse -> clientResponse.bodyToMono(String.class)
-                            .defaultIfEmpty("<no response body>")
-                            .flatMap(body -> Mono.error(new RuntimeException(
-                                    "VisualCrossing API failed with status " + clientResponse.statusCode() + ": "
-                                            + body))))
-                    .bodyToMono(WeatherResponse.class)
-                    .block();
-        } catch (Exception e) {
-            throw new RuntimeException("Weather API call failed: " + e.getMessage(), e);
-        }
-    }
 }
